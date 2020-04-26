d42db7f29fbfe45d4ba89a4d2dc3bde0bb74edc6
Yongseok <qqfelix@naver.com>
2020-04-26 17:31:33 +0900

    proptypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
 
-function App() {
+const foodIlike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/10/10/e1d38d22a01a5f11619e141e089f66cb1.jpg",
+    rating: 5
+  },
+  {
+    id:2,
+    name: "samgiopsal",
+    image:"https://w.namu.la/s/768f1fdb6b65fc9fa7b6e2842523512f1a29bd510e63fe014b150f0ef821824d677696bd7acd360119e14de9bba6774a4c2ed567e81df279513767992f151ef873f335a872dcba470c9025e883c422799f7e467d8d74edb3e9f81bed7722b23dc120fb48348a57ef4fc4068d7f5239a6",
+    rating: 4.8,
+  },
+  {
+    id:3,
+    name: "chukumi",
+    image:"https://recipe1.ezmember.co.kr/cache/recipe/2019/03/05/52d2be99c015378a75c9db81362422c71.jpg",
+    rating: 4.5
+
+  },
+]
+function Food({name, picture,rating}){ 
+  //            props.fav를 사용하는 것과 동일.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> I like {name}</h1>
+  <h4>{rating}/5.0</h4>
+    <img src = {picture} alt={name}></img>
+
     </div>
-  );
+  )
+}
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+} 
+function App() {
+  return <div > 
+    <h1>Food</h1>
+    {foodIlike.map(dish => 
+    <Food 
+    name={dish.name} 
+    picture={dish.image} 
+    key={dish.id}
+    rating={dish.rating}/>)}
+    </div>
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />   
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    {/* 컴포넌트는 기본적으로 html을 반환하는 함수임. 
+      jsx : react의 유일한 html과 js 사이의 이러한 조합/ jsx는 js 안의 html임.component를 만들고 어떻게 사용하는지에 대한 것. 
+    */}
+    {/* jsx 요소는 오직 하나만 가질 수 있음. ex )  <App /><Potato />불가*/}
+  
+  </React.StrictMode>,
+  document.getElementById('potato')
+  // index.html의 root 라는 id를 가진 
+  // 컴포넌트내에 모든 react 파일 상의 html로 만든 것들을 집어넣음
+);
